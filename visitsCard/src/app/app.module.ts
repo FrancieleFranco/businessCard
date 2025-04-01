bb2b8ee8547ad6a97af6b9b606beed993ec1d0d1
Franciele Franco <fran.alves1108@gmail.com>
2025-04-01 17:03:04 -0300

    create header and footer

--- visitsCard/src/app/app.module.ts
+++ visitsCard/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from 'src/components/header/header.component';
+import { FooterComponent } from 'src/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
